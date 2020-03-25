af7884b684b9769b5bb174564279f430280f67a9
Jacob Smith <js@jacobsmith.tech>
2020-03-24 22:18:46 -0700

    Fix seed script

--- seed-haproxy.js
+++ seed-haproxy.js
@@ -4,10 +4,10 @@ const ipPath = './ip-address.txt'
 const templatePath = './template/haproxy.cfg'
 const configPath = './config/haproxy.cfg'
 
-module.exports = async () => {
+;(async () => {
   const ip = await fs.readFile(ipPath, { encoding: 'utf8' })
   let config = await fs.readFile(templatePath, { encoding: 'utf8' })
   config = config.replace(/(server proxy )[\d.]{7,15}(:3535 check # Replace target)/, `$1${ip}$2`)
   await await fs.writeFile(configPath, config)
   console.log('Done\r\n')
-}
+})()
